78b451946a84eb7ff155183426415c8fa3b5227b
matthias mitterreiter <matthias@mitterreiter.de>
2019-05-28 16:21:52 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,29 @@ from Cython.Build import cythonize
 import numpy as np
 # import distutils.sysconfig
 # import platform
+import ssl
 import wget
 import os
 import zipfile
+import sys
 
+if sys.platform.startswith("linux"):
+    pass
+    # os.environ["CC"] = "clang++"
+    # os.environ["CXX"] = "clang++"
+elif sys.platform.startswith("win"):
+    pass
+elif sys.platform.startswith("darwin"):
+    pass
+else:
+    # TODO do not set any flags,
+    # recommend to setup their own compilation script,
+    # if this one fails
+    raise Exception("OS not supported!")
+
+# Notes
+# clang works only with "-std=c++11"
+# what if we are on windows ???
 
 dir_eigen_include = os.path.join("genosolver", "eigen")
 url = "https://github.com/eigenteam/eigen-git-mirror/archive/3.3.7.zip"
@@ -17,7 +36,8 @@ file_eigen_zip = "eigen-git-mirror-3.3.7.zip"
 if not os.path.isdir(dir_eigen_include):
     if not os.path.isfile(file_eigen_zip):
         print("Downloading eigen requirements...")
-        file_eigen_zip = wget.download(url)
+        ssl._create_default_https_context = ssl._create_unverified_context
+        file_eigen_zip = wget.download(url, bar=None)
         print("\n done.")
     print("Extracting files")
     with zipfile.ZipFile(file_eigen_zip, "r") as zip_file:
@@ -27,7 +47,7 @@ if not os.path.isdir(dir_eigen_include):
     os.remove(file_eigen_zip)
 
 
-ext = Extension("genosolver.genointerface",
+ext = Extension(name="genosolver.genointerface",
                 sources=[
                     os.path.join("genosolver", "genointerface.pyx"),
                     os.path.join("genosolver", "pygenointerface.cpp"),
@@ -36,8 +56,8 @@ ext = Extension("genosolver.genointerface",
                     os.path.join("genosolver", "lineSearch.cpp"),
                     os.path.join("genosolver", "augmentedLagrangian.cpp")],
                 language="c++",
-                include_dirs=['.',
-                              "./genosolver",
+                extra_compile_args=["-std=c++11"],
+                include_dirs=["genosolver",
                               np.get_include(),
                               dir_eigen_include]
                 )
@@ -47,12 +67,12 @@ setup(name="genosolver",
       url="http://geno4neurips.pythonanywhere.com",
       author="GENO team",
       license="",
-      # zip_safe=False,
+      zip_safe=False,
       packages=find_packages(),
-      ext_modules=cythonize(ext))
+      ext_modules=cythonize(ext),
+      python_requires=">3.7")
 
-os.remove("genosolver/genointerface.cpp")
-
+os.remove(os.path.join("genosolver", "genointerface.cpp"))
 
 # # from distutils.core import setup, Extension
 # from setuptools import setup, Extension, find_packages
